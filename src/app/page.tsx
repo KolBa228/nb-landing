59ddf110d752cd81049f16dca04d07ab143de3c7
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2025-12-15 12:24:57 +0200

    remove special offer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     return (
         <div className="items-center justify-items-center w-full">
             <main className="flex flex-col row-start-2 items-center sm:items-start w-full">
-                <SpecialOffer />
+                {/* <SpecialOffer /> */}
                 <Hero />
                 <Features />
                 <MainServices />
